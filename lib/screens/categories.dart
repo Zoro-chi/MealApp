3927f4a3c25a82a8e039aba3ffcdb5acc2c9b132
Zoro <jagbetuyi001@gmail.com>
2024-08-23 20:29:49 +0100

    Implemented BottomNavigation tab

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -26,23 +26,19 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Pick Category'),
-        ),
-        body: GridView(
-          padding: const EdgeInsets.all(24),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 1.5,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          children: [
-            for (final category in availableCategories)
-              CategoryGridItem(category, () {
-                _selectCategory(context, category);
-              }),
-          ],
-        ));
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(category, () {
+            _selectCategory(context, category);
+          }),
+      ],
+    );
   }
 }
